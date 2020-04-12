4c8530d0140467c83461c0c35216b0c1cbef96f0
lz82 <nickylau82@gmail.com>
2020-04-12 23:55:20 +0800

    :construction: init redux-thunk

--- src/store/action-creators/todo-list/index.js
+++ src/store/action-creators/todo-list/index.js
@@ -1,34 +1,36 @@
-import { todolistActionTypes  } from "@/store/action-types";
+import { todolistActionTypes } from "@/store/action-types";
+import axios from 'axios'
 
-export function queryTodoListCreator () {
-  return {
-    type: todolistActionTypes.QUERY_TODO_LIST
+export function queryTodoListCreator() {
+  return async (dispatch) => {
+    const res = await axios.get('/mock/list.json')
+    console.log(res.data)
   };
 }
 
-export function todoInputChangeCreator (val) {
+export function todoInputChangeCreator(val) {
   return {
     type: todolistActionTypes.TODO_INPUT_CHANGE,
-    payload: val
+    payload: val,
   };
 }
 
-export function todoInputAddCreator () {
+export function todoInputAddCreator() {
   return {
-    type: todolistActionTypes.TODO_INPUT_ADD
+    type: todolistActionTypes.TODO_INPUT_ADD,
   };
 }
 
-export function toggleTodoStatusCreator (id) {
+export function toggleTodoStatusCreator(id) {
   return {
     type: todolistActionTypes.TOGGLE_TODO_STATUS,
-    payload: id
+    payload: id,
   };
 }
 
-export function changeTodoTabCreator (val) {
+export function changeTodoTabCreator(val) {
   return {
     type: todolistActionTypes.CHANGE_TODO_TAB,
-    payload: val
+    payload: val,
   };
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,18 @@
-import {createStore} from 'redux'
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducer from "./reducers";
 
-import reducer from './reducers'
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
 );
 
-export default store
+const store = createStore(reducer, enhancer);
+
+export default store;
