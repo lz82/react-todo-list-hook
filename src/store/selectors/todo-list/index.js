1427b7147e22399f6c01a3971164122d3307f9cd
lz82 <nickylau82@gmail.com>
2020-04-14 21:49:50 +0800

    :construction: complete reselect

--- src/store/selectors/todo-list/index.js
+++ src/store/selectors/todo-list/index.js
@@ -1,41 +1,33 @@
-import { createSelector } from 'reselect';
+import { createSelector } from "reselect";
 
-<<<<<<< HEAD
-export const getTodoList = state => {
-  return state.getIn(['todolist', 'list'])
-}
-=======
 export const getTodoInputVal = (state) => {
-	return state.getIn(['todolist', 'todoInput']);
+  return state.getIn(["todolist", "todoInput"]);
 };
->>>>>>> c90b83ca499524510b6068f12aa6be0a136aa278
 
 export const getOriginTodoList = (state) => {
-	return state.getIn(['todolist', 'list']).toJS();
+  return state.getIn(["todolist", "list"]);
 };
 
 export const getTodoFilter = (state) => {
-	return state.getIn(['todolist', 'filter']);
+  return state.getIn(["todolist", "filter"]);
 };
 
 export const getLoadingStatus = (state) => {
-	return state.getIn(['todolist', 'isLoading']);
+  return state.getIn(["todolist", "isLoading"]);
 };
 
-export const getTodoList = (state) => {
-	const filterSelector = getTodoFilter;
-	const listSelector = getOriginTodoList;
-
-	return createSelector(filterSelector, listSelector, (filter, list) => {
-		switch (filter) {
-			case 'all':
-				return list;
-			case 'todo':
-				return list.filter((item) => !item.complete);
-			case 'complete':
-				return list.filter((item) => item.complete);
-			default:
-				return list;
-		}
-	});
-};
+export const getTodoList = createSelector(
+  [getTodoFilter, getOriginTodoList],
+  (filter, list) => {
+    switch (filter) {
+      case "all":
+        return list;
+      case "todo":
+        return list.filter((item) => !item.complete);
+      case "complete":
+        return list.filter((item) => item.complete);
+      default:
+        return list;
+    }
+  }
+);
